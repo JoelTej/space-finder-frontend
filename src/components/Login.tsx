af34126e79fac38dd07456b46037ef2db93efaa5
Joel Tejada <joel.tejada@estudiantat.upc.edu>
2022-05-06 13:09:22 +0200

    Add files via upload

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import { AuthService } from "../services/AuthService";
 
 
@@ -6,11 +6,16 @@ import { AuthService } from "../services/AuthService";
 interface LoginProps {
     authService: AuthService
 }
+
 interface LoginState {
     userName: string,
     password: string,
     loginAttenpted: boolean,
     loginSuccesfull: boolean
+}
+
+interface CustomEvent {
+    target: HTMLInputElement
 }
 
 export class Login extends React.Component<LoginProps, LoginState> {
@@ -22,14 +27,34 @@ export class Login extends React.Component<LoginProps, LoginState> {
         loginSuccesfull: false
     }
 
+    private setUserName(event: CustomEvent) {
+        this.setState({ userName: event.target.value })
+    }
+
+    private setPassword(event: CustomEvent) {
+        this.setState({ password: event.target.value })
+    }
+
+    private async handleSubmit(event: SyntheticEvent) {
+        event.preventDefault();
+        const result = await this.props.authService.login(
+            this.state.userName,
+            this.state.password
+        )
+        if (result) {
+            console.log(result)
+        } else {
+            console.log('wrong login')
+        }
+    }
 
     render() {
         return (
             <div>
                 <h2>Please login</h2>
-                <form>
-                    <input value={this.state.userName} /><br />
-                    <input value={this.state.password} type='password' /><br />
+                <form onSubmit={e => this.handleSubmit(e)}>
+                    <input value={this.state.userName} onChange={e => this.setUserName(e)} /><br />
+                    <input value={this.state.password} onChange={e => this.setPassword(e)} type='password' /><br />
                     <input type='submit' value='Login' />
                 </form>
             </div>
